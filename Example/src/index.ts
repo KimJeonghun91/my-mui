45c58f5083418f699d01d3c4660f848e4f978b61
righthot <righthot@everon.co.kr>
2024-03-15 14:42:20 +0900

    tsconfig 설정변경 - npm 사용 테스트 완료

--- Example/src/index.ts
+++ Example/src/index.ts
@@ -1,6 +1,4 @@
-import * as material from '@mui/material';
-import * as xDatePickers from '@mui/x-date-pickers';
 import * as widgets from './widgets';
 import * as shared from './shared';
 
-export { material, xDatePickers, widgets, shared };
+export { widgets, shared };
