49cd578399036f49b429b427c0d8989794a2906d
righthot <righthot@everon.co.kr>
2024-03-18 19:02:50 +0900

    feat: useLogin 분리

--- Example/src/app/login/page.tsx
+++ Example/src/app/login/page.tsx
@@ -1,45 +1,15 @@
 'use client';
 
 import { styled } from "@mui/material";
-import * as yup from 'yup';
 import RootView from "../../../lib/shared/ui/rootView";
 import Form from "../../../lib/widgets/Form";
+import { initialValues, validationSchema } from "./model/useLogin";
 import { FormValues } from "../../../lib/widgets/Form/model/types";
 
 export default function Login() {
-
-  // TODO: data 폴더로 빼야됨. 폴더명 data로 할지도 고민
-
-  const initialValues = {
-    email: {
-      id: 'email',
-      value: '',
-      label: '이메일',
-      type: 'text'
-    },
-    password: {
-      id: 'password',
-      value: '',
-      label: '비밀번호',
-      type: 'password'
-    },
-  }
-
-  const validationSchema = yup.object({
-    email: yup
-      .string()
-      .email('Enter a valid email')
-      .required('Email is required'),
-    password: yup
-      .string()
-      .min(8, 'Password should be of minimum 8 characters length')
-      .required('Password is required'),
-  });
-
   const handleSubmit = (values: FormValues) => {
     console.log(values);
   }
-
 
   return (
     <RootView>
